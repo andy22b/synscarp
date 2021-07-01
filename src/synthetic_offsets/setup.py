a33b1bbe181a7b74e0115b97f7d84b13248302bd
andy22b <andy22b@gmail.com>
2021-07-02 09:05:05 +1200

    populate setup.py

---
+++ src/synthetic_offsets/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import sys
+
+install_requires = ["numpy>=1.17",
+                    "matplotlib>=3.1.1",
+                    "geopandas>=0.6.1",
+                    "netcdf4>=1.4.2",
+                    "ipython>=7.9.0",
+                    "scipy>=1.3.1",
+                    "rasterio>=1.1.0",
+                    "jupyterlab>=3.0.9"]
+
+setup(name='synthetic_offsets',
+      version='0.0.1',
+      description='Simulation of offsets',
+      author='Andy Howell',
+      author_email='andrew.howell@canterbury.ac.nz',
+      packages=find_packages(),
+      install_requires=install_requires
+      )
